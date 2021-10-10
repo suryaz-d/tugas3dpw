bd6eeb51f8ab38864c5b6b135d17592a367cf87a
suryaz-d <91340611+suryaz-d@users.noreply.github.com>
2021-10-10 23:42:17 +0700

    Tugas 2 DPW2

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+
+Route::get('/why', function () {
+    return view('why');
+});
+
+Route::get('/admin', function () {
+    return view('layout/admin');
+});
